bbee1031be66153632abfd7217e2490e18d4dacc
silvakwan1 <123783319+silvakwan1@users.noreply.github.com>
2023-12-18 09:09:07 -0300

    up date

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3100;
 connectToMongoDB();
 
-// const routerPost = require("./router/routerPost");
-// const routerPut = require("./router/routerPut");
+const routerPost = require("./router/routerPost");
+const routerPut = require("./router/routerPut");
 const routerGet = require("./router/routerGet");
 
 require("dotenv").config();
@@ -16,8 +16,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.use(routerPost);
-// app.use(routerPut);
+app.use(routerPost);
+app.use(routerPut);
 app.use(routerGet);
 
 app.listen(PORT, () => console.log(`servidor rodando na porta ${PORT}`));
